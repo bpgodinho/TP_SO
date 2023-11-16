a2f57e355d57b86c4f0910e0b367bda8912315ae
Rafael <rafaelgomespinto04@gmail.com>
2023-11-16 04:02:20 +0000

    TRINTA CARALHO
    AAAAAAAAAAAAAAA

--- motor.c
+++ motor.c
@@ -1,52 +1,29 @@
 #include "motor.h"
 
-void mostraLabirinto(char labirinto[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas, WINDOW *window) {
+void mostrarLabirinto(char labirinto[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas) {
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
-            mvwaddch(window, labirinto[i][j]);
+            printf("%c", labirinto[i][j]);
         }
         printf("\n");
     }
-    addch(window, "refresh");
-    refresh();
 }
 
 int main() {
-    WINDOW *window;
-    initscr();
-    start_color();
-    erase();
-    noecho();
-    cbreak();
-    addch(window, "inicio");
     char labirinto[MAX_LINHAS][MAX_COLUNAS];
     int linhas = 0, colunas = 0;
-
-    // Open the file for reading
     FILE *file = fopen("labirinto.txt", "r");
     if (file == NULL) {
-        perror("Erro a abrir ficheiro");
+        perror("Erro a abrir o ficheiro");
         return 1;
     }
-
-    // Read the labirinto from the file
     while (fgets(labirinto[linhas], MAX_COLUNAS, file) != NULL) {
-        // Remove the newline character if present
         labirinto[linhas][strcspn(labirinto[linhas], "\n")] = '\0';
+        printf("Line %d length: %zu\n", linhas + 1, strlen(labirinto[linhas]));
         linhas++;
     }
-
-    // Close the file
     fclose(file);
-
-    // Determine the number of columns
     colunas = strlen(labirinto[0]);
-
-    // Print the labirinto
-    mostraLabirinto(labirinto, linhas, colunas, window);
-
-    while(1){
-        printf("teste");
-    }
+    mostrarLabirinto(labirinto, linhas, colunas);
     return 0;
 }
--- motor.h
+++ motor.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <ncurses.h>
+#include <string.h>
 
 #define MAX_LINHAS 16
-#define MAX_COLUNAS 40
-void mostraLabirinto(char labirinto[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas, WINDOW *window);
+#define MAX_COLUNAS 43
+void mostrarLabirinto(char labirinto[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas);
